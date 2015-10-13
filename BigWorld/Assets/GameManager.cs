d790f5fde353a4bdc57483649d4933bce6063946
Kunjky <nguyenviet.hai1802@gmail.com>
2015-10-13 18:07:07 +0700

    Upload Unity Project

---
+++ BigWorld/Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager Instance{get;private set;}
+	public GameObject grounds;
+	public float groundMoveSpeed;
+
+	void Awake(){
+		Instance=this;
+	}
+	void Start () {
+		grounds.GetComponent<Rigidbody>().velocity=Vector3.back*groundMoveSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void EndGame(){
+		grounds.GetComponent<Rigidbody>().velocity=Vector3.zero;
+	}
+}
---
+++ BigWorld/Assets/Scripts/CynlinderRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CynlinderRotate : MonoBehaviour {
+	public float speedRotate;
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.rotation=Quaternion.Euler(transform.rotation.x,transform.rotation.y,Input.GetAxis("Horizontal")*10);
+		transform.Rotate(transform.rotation.x,transform.rotation.y,Input.GetAxis("Horizontal")*speedRotate);
+	}
+}
